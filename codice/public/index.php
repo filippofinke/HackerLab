3072b22b5bab7abc3d29f837ab2ca01d5f630434
Filippo Finke <filippo.finke@samtrevano.ch>
2019-09-17 16:11:29 +0200

    Implemented backend logic

--- codice/public/index.php
+++ codice/public/index.php
@@ -14,8 +14,34 @@ session_start();
 $settings = array(
     'settings' => [
         'displayErrorDetails' => true
+    ],
+    'database' => [
+        'host' => '127.0.0.1',
+        'user' => 'root',
+        'password' => 'filospinato22',
+        'dbname' => 'hackerlab'
     ]
 );
+
+class Database {
+
+    private static $connection;
+
+    public static function get() {
+        return self::$connection;
+    }
+
+    public static function connect($settings) {
+        self::$connection = new PDO('mysql:host='.$settings["host"].';dbname='.$settings["dbname"], $settings["user"], $settings["password"], array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ));
+    }
+
+}
+Database::connect($settings["database"]);
+require __DIR__ . '/../src/models/users.php';
+require __DIR__ . '/../src/models/articles.php';
+require __DIR__ . '/../src/models/comments.php';
 
 $app = new \Slim\App($settings);
 
--- codice/src/routes.php
+++ codice/src/routes.php
@@ -6,20 +6,114 @@ use Slim\Http\Response;
 
 return function (App $app) {
 
-    $app->get('/', function (Request $request, Response $response, array $args) use ($app) {
-        $this->view->render($response, "index.phtml", $args);
+
+    $login_middleware = function ($request, $response, $next) {
+        if (!isset($_SESSION["user"])) {
+            $_SESSION["big_error"] = "Per eseguire questa azione devi accedere!";
+            return $response->withRedirect("/", 302);
+        }
+        $response = $next($request, $response);
+        return $response;
+    };
+
+    /**
+     * Chiamate
+     */
+
+    $app->get('/logout', function (Request $request, Response $response, array $args) use ($app) {
+        unset($_SESSION["user"]);
+        session_destroy();
+        setcookie("permission", '', time() - 3600);
+        return $response->withRedirect("/", 302);
+    });
+
+    $app->post('/login', function (Request $request, Response $response, array $args) use ($app) {
+        $email = $request->getParam('email');
+        $password = $request->getParam('password');
+        Users::login($email, $password);
+        return $response->withRedirect("/", 302);
+    });
+
+    /**
+     * Pagine
+     */
+    $app->get('/[page/{page}]', function (Request $request, Response $response, array $args) use ($app) {
+        $permission = isset($_COOKIE["permission"])?base64_decode($_COOKIE["permission"]):null;
+        $page = $args["page"] ?? 0;
+        $articles = [];
+        if($request->getParam('search') !== null) {
+            $articles = Articles::search($request->getParam('search'), $page);
+        } else {
+            $articles = Articles::get($page);
+        }
+        if(count($articles) == 0 && $page != 0) {
+            return $response->withRedirect("/page/".($page - 1), 302);
+        }
+        $this->view->render($response, "index.phtml", array(
+            'permission' => $permission,
+            'articles' => $articles,
+            'page' => $page
+        ));
     });
     
     $app->get('/register', function (Request $request, Response $response, array $args) use ($app) {
+        if(isset($_SESSION["user"])) return $response->withRedirect("/", 302);
         $this->view->render($response, "register.phtml", $args);
     });
 
-    $app->get('/profile', function (Request $request, Response $response, array $args) use ($app) {
-        $this->view->render($response, "profile.phtml", $args);
+    /**
+     * Pagine che richiedono l'accesso.
+     */
+    $app->get('/profile[/{user_id}]', function (Request $request, Response $response, array $args) use ($app) {
+        $permission = isset($_COOKIE["permission"])?base64_decode($_COOKIE["permission"]):null;
+        $user_id = $args["user_id"] ?? $_SESSION["user"]["id"];
+        $user = Users::getById($user_id);
+        if ($user) {
+            $articles = Articles::getByUserId($user_id);
+            $this->view->render($response, "profile.phtml", array(
+                'permission' => $permission,
+                'user' => $user,
+                'articles' => $articles
+            ));
+        } else {
+            return $response->withRedirect("/", 302);
+        }
+    })->add($login_middleware);
+
+    $app->get('/post/{post_id}', function (Request $request, Response $response, array $args) use ($app) {
+        $permission = isset($_COOKIE["permission"])?base64_decode($_COOKIE["permission"]):null;
+        $post = Articles::getById($args["post_id"]);
+        if ($post) {
+            $this->view->render($response, "post.phtml", array(
+                'permission' => $permission,
+                'post' => $post,
+                'comments' => Comments::getByArticleId($post["id"])
+            ));
+        } else {
+            return $response->withRedirect("/", 302);
+        }
+    })->add($login_middleware);
+
+    $app->post('/post/{post_id}', function (Request $request, Response $response, array $args) use ($app) {
+        $comment = $request->getParam("comment");
+        $post = Articles::getById($args["post_id"]);
+        if ($post) {
+            if (preg_replace('/\s+/', '', $comment) != "") {
+                Comments::insert($post["id"], $_SESSION["user"]["id"], $comment);
+            } else {
+                $_SESSION["error"] = "Il commento non può essere vuoto!";
+            }
+            return $response->withRedirect("/post/".$args["post_id"], 302);
+        }
+        return $response->withRedirect("/", 302);
+    })->add($login_middleware);
+
+    $app->get('/admin/articoli', function (Request $request, Response $response, array $args) use ($app) {
+        $this->view->render($response, "admin/articoli.phtml", $args);
     });
 
-    $app->get('/post', function (Request $request, Response $response, array $args) use ($app) {
-        $this->view->render($response, "post.phtml", $args);
+    $app->get('/admin/utenti', function (Request $request, Response $response, array $args) use ($app) {
+        $this->view->render($response, "admin/utenti.phtml", $args);
     });
 
 };
